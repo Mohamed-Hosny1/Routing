c7cb12f077d0f41541a36357a288692e84371bbc
Mohamed-Hosny1 <hosny0169@gmail.com>
2025-01-13 01:44:48 +0200

    First commit

---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { log } from 'node:console';
+
+@Component({
+  selector: 'app-portfolio',
+  imports: [],
+  templateUrl: './portfolio.component.html',
+  styleUrl: './portfolio.component.css',
+})
+export class PortfolioComponent {}
